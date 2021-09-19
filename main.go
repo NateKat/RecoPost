4b0c1f795f41944d05dfd8c6b6ca0828d3934f65
Netanel Katzburg <netanel10k@gmail.com>
2021-09-19 17:31:25 +0300

    main: Added a scanner for input
    Handling of basic errors.
    A loop for each city according to input

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Num of cities: ")
+	scanner.Scan() // Scans a line from Stdin(Console)
+
+	text := scanner.Text()
+	if len(text) == 0 {
+		fmt.Println("Error: empty input")
+	}
+	fmt.Println(text, "cities") // Debug
+
+	num_cities, err := strconv.Atoi(text)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := 0; i < num_cities; i++ {
+		// TODO: new city
+	}
+	if scanner.Err() != nil {
+		// Handle error.
+	}
+
 }
